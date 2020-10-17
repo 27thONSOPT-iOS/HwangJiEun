72b0aecb404da9ba4f3e3726d3a29970188ef8ff
hwangji-dev <melon2228@naver.com>
2020-10-17 16:56:53 +0900

    2주차 세미나 실습 업로드

---
+++ 2nd_Seminar_27th/2nd_Seminar_27th/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd_Seminar_27th
+//
+//  Created by 황지은 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
